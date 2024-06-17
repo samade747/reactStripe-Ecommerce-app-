c8dddc2b298ae93102be5b02f97bee52bb861217
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-06-17 11:07:15 +0500

    Update Success.jsx

---
+++ vite-project/src/Success.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+import { Pay } from './Pay'
+import { Success } from './Success'
+
+
+const Success = () => {
+    return (
+        <div>
+            Success
+        </div>
+    )
+}
